364d4a27187579fa36420aa7c6df02b540bddf1d
escobarnatalia <60725004+escobarnatalia@users.noreply.github.com>
2020-09-17 19:36:19 -0500

    Pantallas listas

--- app/src/main/java/co/natalia/quiz1eco/MainActivity.java
+++ app/src/main/java/co/natalia/quiz1eco/MainActivity.java
@@ -3,12 +3,24 @@ package co.natalia.quiz1eco;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textEncuestas;
+    private TextView textInfo;
+    private Button registrarBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textInfo =findViewById(R.id.textInfo);
+        textEncuestas = findViewById(R.id.textEncuestas);
+        registrarBtn = findViewById(R.id.registrarBtn);
+
+
     }
 }
